404e5056ae687e1401cc79730e4754b1fe491d19
LiuTongshuo <lqyhk1234@sina.com>
2018-03-18 21:33:43 +0800

    Change some function names

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,19 @@
 extern crate proc_macro_hack;
 
 proc_macro_expr_impl! {
-    pub fn noprefix_hex_u8(input: &str) -> String {
+    pub fn noprefix_hex_length_2_into_u8_impl(input: &str) -> String {
         format!("0x{}u8", input)
     }
 
-    pub fn noprefix_hex_u16(input: &str) -> String {
+    pub fn noprefix_hex_length_4_into_u16_impl(input: &str) -> String {
         format!("0x{}u16", input)
     }
 
-    pub fn noprefix_hex_u32(input: &str) -> String {
+    pub fn noprefix_hex_length_8_into_u32_impl(input: &str) -> String {
         format!("0x{}u32", input)
     }
 
-    pub fn noprefix_hex_u64(input: &str) -> String {
+    pub fn noprefix_hex_length_16_into_u64_impl(input: &str) -> String {
         format!("0x{}u64", input)
     }
 }
